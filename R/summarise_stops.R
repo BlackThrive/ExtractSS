6430707ff1e4345dbeee2e5d41a9cac22842925d
Jolyon Miles-Wilson <jolyon.miles-wilson@blackthrive.org>
2023-08-11 16:28:22 +0100

    altered aggregated ethnicities to include differentiated mixed categories

--- R/summarise_stops.R
+++ R/summarise_stops.R
@@ -13,8 +13,17 @@
 #' @param by_ethnicity Boolean indicating whether to analyse by ethnicity or not.
 #' If true, will group data by ethnicity before summarising. Default is FALSE.
 #' @param ethnicity_definition If by_ethnicity is TRUE, ethnicity_definition must be
-#' specified. "self" = self-defined; "officer" = officer-defined. Default is
-#' officer-defined.
+#' specified. "self" = self-defined; "officer" = officer-defined. "combined" will
+#' use self-defined and where self-defined is missing will use officer-defined.
+#' This reduces the number of missing cases (by approximately 24%).
+#' Default is officer-defined.
+#' @param aggregate_ethnicity If using self-defined ethnicity, choose whether
+#' to use the original categories or to aggregate into super-ordinate categories
+#' (i.e., "Black", "White","Asian","Mixed","Other"). Default is FALSE. Note that
+#' mixed Black sub-ordinate categories will be grouped into the "Black"
+#' super-ordinate category. This is based on the assumption that anyone who can be
+#' racialised as Black will have an experience closer to the Black experience than
+#' the non-Black experience.
 #' @param plot If by_area is FALSE, by default the function will produce summary
 #' plots. This can be changed by setting plot = FALSE.
 #' @param save_plot If plot is TRUE, the plot can be saved to png with save_plot.
@@ -42,8 +51,11 @@
 summarise_stops <- function(data,
                               metric = c("object_of_search", "legislation","outcome"),
                               by_area = TRUE,
+                              area_type = "la",
+                              by_year = FALSE,
                               by_ethnicity = FALSE,
-                              ethnicity_definition = c("officer", "self"),
+                              ethnicity_definition = c("officer", "self", "combined"),
+                              aggregate_ethnicity = FALSE,
                               plot = TRUE,
                               save_plot = FALSE,
                               metric_by_ethnicity = FALSE){
@@ -55,7 +67,33 @@ summarise_stops <- function(data,
   # Take metric name for insertion {{}}
   metric <- as.name(metric) # make argument a name so that {{}} understands it
 
+  if(area_type == "la"){
+    area_name <- as.name("la_name")
+    area_code <- as.name("la_code")
+  }
+  else if(area_type == "pfa"){
+    area_name <- as.name("force")
+    # area_code <- as.name("PFA22CD") # this needs to be added to the data
+  }
   data <- data
+  if(by_year == T){
+    data$year <- as.numeric(substr(data$date, 1, 4))
+  }
+
+
+
+  # if(by_year == TRUE){
+  #   # separate date into separate columns
+  #   data$year <- as.numeric(substr(data$date, 1, 4))
+  #   data$month <- as.numeric(substr(data$date, 6, 7))
+  #   data$day <- as.numeric(substr(data$date, 9, 10))
+  #
+  #   # make year_month variable for indexing
+  #   data$year_month <-
+  #     as.Date(paste(
+  #       as.character(data$year),
+  #       as.character(data$month),"01", sep= "-"))
+  # }
 
   ### END 0 ###
 
@@ -64,9 +102,9 @@ summarise_stops <- function(data,
   if(!is.object(data)){
     stop("Data must be a data frame object")
   }
-  # if(by_ethnicity == TRUE & length(ethnicity_definition) != 1){
-  #   stop("Please specify which ethnicity definition to use - 'self' or 'officer'")
-  # }
+  if(by_ethnicity == TRUE & length(ethnicity_definition) != 1){
+    stop("Please specify which ethnicity definition to use - 'self', 'officer', or 'combined'")
+  }
   if(metric_by_ethnicity == T & by_ethnicity == F){
     stop("by_ethnicity must be TRUE for metric_by_ethnicity to apply")
   }
@@ -74,43 +112,92 @@ summarise_stops <- function(data,
     stop("plot must be TRUE for save_plot to apply")
   }
 
-  # set default ethnicity definition
-  if(by_ethnicity == TRUE){
-    ethnicity_definition <- "officer"
-  }
 
   ### END 1 ###
 
-
+  ###
   ### 2 Summarisation ###
 
-  ### 2.1 Overall (i.e., not by area ###
+  ### 2.1 Overall (i.e., not by area) ###
   if(by_area == FALSE){
 
     ### Overall ###
 
     if(by_ethnicity == FALSE){ # Aggregated
       summary_tab <- data %>%
-        group_by({{ metric }}) %>%
-        summarise(
+        dplyr::group_by(if(by_year == T) year, {{ metric }}) %>%
+        dplyr::summarise(
           frequency = n()
         ) %>%
-        mutate(
+        dplyr::mutate(
           percentage = 100 * (frequency / sum(frequency))
         )
     }else if(by_ethnicity == TRUE){ # by ethnicity
       # Assign the desired ethnicity type
       if(ethnicity_definition == "self"){
         data$ethnicity <- data$self_defined_ethnicity
+        if(aggregate_ethnicity == TRUE){
+          data$ethnicity <- as.factor(data$ethnicity)
+          data$ethnicity <- forcats::fct_collapse(data$ethnicity,
+                                                  Asian = c("Asian/Asian British - Any other Asian background",
+                                                            "Asian/Asian British - Bangladeshi",
+                                                            "Asian/Asian British - Chinese",
+                                                            "Asian/Asian British - Indian",
+                                                            "Asian/Asian British - Pakistani"),
+                                                  Black = c("Black/African/Caribbean/Black British - African",
+                                                            "Black/African/Caribbean/Black British - Any other Black/African/Caribbean background",
+                                                            "Black/African/Caribbean/Black British - Caribbean"
+                                                  ),
+                                                  Mixed_Asian = c("Mixed/Multiple ethnic groups - White and Asian"),
+                                                  Mixed_Black = c("Mixed/Multiple ethnic groups - White and Black African",
+                                                                  "Mixed/Multiple ethnic groups - White and Black Caribbean"),
+                                                  Mixed_Other = c("Mixed/Multiple ethnic groups - Any other Mixed/Multiple ethnic background"),
+                                                  Other = c("Other ethnic group - Any other ethnic group",
+                                                            "Other ethnic group - Not stated"),
+                                                  White = c("White - Any other White background",
+                                                            "White - English/Welsh/Scottish/Northern Irish/British",
+                                                            "White - Irish")
+          )
+        }
       }else if(ethnicity_definition == "officer"){
         data$ethnicity <- data$officer_defined_ethnicity
       }
+      else if(ethnicity_definition == "combined"){ # otherwise combine self and officer defined
+        # make self-defined a factor then collapse
+        data$self_defined_ethnicity <- as.factor(data$self_defined_ethnicity)
+        data$ethnicity <- forcats::fct_collapse(data$ethnicity,
+                                                Asian = c("Asian/Asian British - Any other Asian background",
+                                                          "Asian/Asian British - Bangladeshi",
+                                                          "Asian/Asian British - Chinese",
+                                                          "Asian/Asian British - Indian",
+                                                          "Asian/Asian British - Pakistani"),
+                                                Black = c("Black/African/Caribbean/Black British - African",
+                                                          "Black/African/Caribbean/Black British - Any other Black/African/Caribbean background",
+                                                          "Black/African/Caribbean/Black British - Caribbean"
+                                                ),
+                                                Mixed_Asian = c("Mixed/Multiple ethnic groups - White and Asian"),
+                                                Mixed_Black = c("Mixed/Multiple ethnic groups - White and Black African",
+                                                                "Mixed/Multiple ethnic groups - White and Black Caribbean"),
+                                                Mixed_Other = c("Mixed/Multiple ethnic groups - Any other Mixed/Multiple ethnic background"),
+                                                Other = c("Other ethnic group - Any other ethnic group",
+                                                          "Other ethnic group - Not stated"),
+                                                White = c("White - Any other White background",
+                                                          "White - English/Welsh/Scottish/Northern Irish/British",
+                                                          "White - Irish")
+        )
+        # set missing self-defined ethnicity to officer defined ethnicity
+        data$ethnicity[is.na(data$ethnicity)] <- data$officer_defined_ethnicity[is.na(data$ethnicity)]
+        # keep record of cases where self and officer defined have been merged
+        data$ethnicity_changed <- rep(0, nrow(data))
+        data$ethnicity_changed[is.na(data$self_defined_ethnicity) & !is.na(data$officer_defined_ethnicity)] <- 1
 
+
+      }
       # express percentages as ethnicity within reason
       # i.e., "18% of stops of for controlled drugs are stops of Asians"
       if(metric_by_ethnicity == T){
         summary_tab <- data %>%
-          group_by({{ metric }}, ethnicity) %>% # ethnicity within reason
+          group_by(if(by_year == T) year, {{ metric }}, ethnicity) %>% # ethnicity within reason
           summarise(
             frequency = n()
           ) %>%
@@ -122,7 +209,7 @@ summarise_stops <- function(data,
       # i.e., "74% of stops of Asians are for controlled drugs"
       }else{
         summary_tab <- data %>%
-          group_by(ethnicity, {{ metric }}) %>% # reason within ethnicity
+          group_by(if(by_year == T) year, ethnicity, {{ metric }}) %>% # reason within ethnicity
           summarise(
             frequency = n()
           ) %>%
@@ -183,7 +270,14 @@ summarise_stops <- function(data,
   }else{
     if(by_ethnicity == FALSE){ # Aggregated
       summary_tab <- data %>%
-        group_by(la_name, {{ metric }}) %>%
+        group_by(if(by_year == T) year,
+                 {{ area_name }},
+                 # if(area_type == "la" ) {{ area_code }},
+                 if(area_type == "la") county,
+                 if(area_type == "la") region,
+                 if(area_type == "la") country,
+                 if(area_type == "la") force,
+                 {{ metric }}) %>%
         summarise(
           frequency = n()
         ) %>%
@@ -194,15 +288,78 @@ summarise_stops <- function(data,
       # Assign the desired ethnicity type
       if(ethnicity_definition == "self"){
         data$ethnicity <- data$self_defined_ethnicity
+        if(aggregate_ethnicity == TRUE){
+          data$ethnicity <- as.factor(data$ethnicity)
+          data$ethnicity <- forcats::fct_collapse(data$ethnicity,
+                                                  Asian = c("Asian/Asian British - Any other Asian background",
+                                                            "Asian/Asian British - Bangladeshi",
+                                                            "Asian/Asian British - Chinese",
+                                                            "Asian/Asian British - Indian",
+                                                            "Asian/Asian British - Pakistani"),
+                                                  Black = c("Black/African/Caribbean/Black British - African",
+                                                            "Black/African/Caribbean/Black British - Any other Black/African/Caribbean background",
+                                                            "Black/African/Caribbean/Black British - Caribbean"
+                                                  ),
+                                                  Mixed_Asian = c("Mixed/Multiple ethnic groups - White and Asian"),
+                                                  Mixed_Black = c("Mixed/Multiple ethnic groups - White and Black African",
+                                                                  "Mixed/Multiple ethnic groups - White and Black Caribbean"),
+                                                  Mixed_Other = c("Mixed/Multiple ethnic groups - Any other Mixed/Multiple ethnic background"),
+                                                  Other = c("Other ethnic group - Any other ethnic group",
+                                                            "Other ethnic group - Not stated"),
+                                                  White = c("White - Any other White background",
+                                                            "White - English/Welsh/Scottish/Northern Irish/British",
+                                                            "White - Irish")
+          )
+        }
       }else if(ethnicity_definition == "officer"){
         data$ethnicity <- data$officer_defined_ethnicity
       }
+      else if(ethnicity_definition == "combined"){ # otherwise combine self and officer defined
+        # make self-defined a factor then collapse
+        data$self_defined_ethnicity <- as.factor(data$self_defined_ethnicity)
+        data$ethnicity <- forcats::fct_collapse(data$ethnicity,
+                                                Asian = c("Asian/Asian British - Any other Asian background",
+                                                          "Asian/Asian British - Bangladeshi",
+                                                          "Asian/Asian British - Chinese",
+                                                          "Asian/Asian British - Indian",
+                                                          "Asian/Asian British - Pakistani"),
+                                                Black = c("Black/African/Caribbean/Black British - African",
+                                                          "Black/African/Caribbean/Black British - Any other Black/African/Caribbean background",
+                                                          "Black/African/Caribbean/Black British - Caribbean"
+                                                ),
+                                                Mixed_Asian = c("Mixed/Multiple ethnic groups - White and Asian"),
+                                                Mixed_Black = c("Mixed/Multiple ethnic groups - White and Black African",
+                                                                "Mixed/Multiple ethnic groups - White and Black Caribbean"),
+                                                Mixed_Other = c("Mixed/Multiple ethnic groups - Any other Mixed/Multiple ethnic background"),
+                                                Other = c("Other ethnic group - Any other ethnic group",
+                                                          "Other ethnic group - Not stated"),
+                                                White = c("White - Any other White background",
+                                                          "White - English/Welsh/Scottish/Northern Irish/British",
+                                                          "White - Irish")
+        )
+        # set missing self-defined ethnicity to officer defined ethnicity
+        data$ethnicity[is.na(data$ethnicity)] <- data$officer_defined_ethnicity[is.na(data$ethnicity)]
+        # keep record of cases where self and officer defined have been merged
+        data$ethnicity_changed <- rep(0, nrow(data))
+        data$ethnicity_changed[is.na(data$self_defined_ethnicity) & !is.na(data$officer_defined_ethnicity)] <- 1
 
+
+      }
       # express percentages as ethnicity within reason
       # i.e., "18% of stops of for controlled drugs are stops of Asians"
       if(metric_by_ethnicity == TRUE){
         summary_tab <- data %>%
-          group_by(la_name, {{ metric }}, ethnicity) %>% # ethnicity within reason
+          # ethnicity within reason
+          group_by(if(by_year == T) year,
+                   {{ area_name }},
+                   # if(area_type == "la") {{ area_code }},
+                   if(area_type == "la") county,
+                   if(area_type == "la") region,
+                   if(area_type == "la") country,
+                   if(area_type == "la") force,
+                   {{ metric }},
+                   ethnicity) %>%
+
           summarise(
             frequency = n()
           ) %>%
@@ -214,7 +371,17 @@ summarise_stops <- function(data,
         # i.e., "74% of stops of Asians are for controlled drugs"
       }else{
         summary_tab <- data %>%
-          group_by(la_name, ethnicity, {{ metric }}) %>% # reason within ethnicity
+          # reason within ethnicity
+          group_by(if(by_year == T) year,
+                   {{ area_name }},
+                   # if(area_type == "la") {{ area_code }},
+                   if(area_type == "la") county,
+                   if(area_type == "la") region,
+                   if(area_type == "la") country,
+                   if(area_type == "la") force,
+                   ethnicity,
+                   {{ metric }}) %>%
+
           summarise(
             frequency = n()
           ) %>%
@@ -234,6 +401,29 @@ summarise_stops <- function(data,
   print(paste0("Total stops: ", sum(summary_tab$frequency)))
   print(paste0("Time period: ", min(data$date), " to ", max(data$date)))
   print(paste0("Number of areas: ", length(unique(data$la_name))))
+  if(by_ethnicity == TRUE){
+    total_nas <- sum(is.na(data$ethnicity))
+    perc_nas <- round(100  * (total_nas / nrow(data)), 0)
+    if(ethnicity_definition == "self" | ethnicity_definition == "officer"){
+      print(paste0("Outputs based on ",
+                   ethnicity_definition,
+                   "-defined ethnicity. ",
+                   total_nas,
+                   " (",
+                   perc_nas, "%) of ethnicity cases are NA."))
+    }
+    else{
+      print(paste0("Outputs based on combination of self- and officer-defined ethnicity.",
+                   total_nas,
+                   " (",
+                   perc_nas, "%) of cases are NA."))
+    }
+  }
+
+  # clear up year column name
+  if(by_year == T){
+    colnames(summary_tab)[1] <- "year"
+  }
 
   # return the summarised tibble
   return(summary_tab)
